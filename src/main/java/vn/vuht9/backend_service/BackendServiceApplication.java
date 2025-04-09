c1f21a731c61b0abb0e5658d7de29a10493a0bd0
TuanVu <tuanvu@TuanVus-MacBook-Pro.local>
2025-04-09 20:22:21 +0700

    build project base

--- src/main/java/vn/vuht9/backend_service/BackendServiceApplication.java
+++ src/main/java/vn/vuht9/backend_service/BackendServiceApplication.java
@@ -3,11 +3,12 @@ package vn.vuht9.backend_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BackendServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendServiceApplication.class, args);
-	}
+	}	
 
 }
---
+++ src/main/java/vn/vuht9/backend_service/controller/HelloController.java
@@ -0,0 +1,21 @@
+package vn.vuht9.backend_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/test")
+public class HelloController {
+    @Operation(summary = "test API")
+    @GetMapping("hello")
+    public String getMethodName(@RequestParam String param) {
+        return new String("hello nha");
+    }
+    
+}
