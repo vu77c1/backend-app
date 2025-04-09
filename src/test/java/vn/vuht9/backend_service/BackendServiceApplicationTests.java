b61e6375cdb3a5ee75b4668564bdeb35af5d05d9
TuanVu <tuanvu@TuanVus-MacBook-Pro.local>
2025-04-09 12:18:36 +0700

    first commit

---
+++ src/test/java/vn/vuht9/backend_service/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.vuht9.backend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
