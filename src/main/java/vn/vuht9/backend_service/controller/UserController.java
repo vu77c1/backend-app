e480e456c6e3e29411dedb106c538f24579b716c
TuanVu <tuanvu@TuanVus-MacBook-Pro.local>
2025-04-13 12:09:01 +0700

    config elk

--- src/main/java/vn/vuht9/backend_service/controller/UserController.java
+++ src/main/java/vn/vuht9/backend_service/controller/UserController.java
@@ -1,5 +1,6 @@
 package vn.vuht9.backend_service.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +9,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class UserController {
     @Operation(summary = "test API")
     @GetMapping("hello")
     public String getMethodName(@RequestParam String param) {
+        log.info("helooo");
         return new String("hello nha");
     }
     
